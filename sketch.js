4cd5729f3026a9c8e4220a7902022aac68df4477
haarika kannan <haarikannan@gmail.com>
2020-11-08 14:18:23 +0530

    vyom

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function setup(){
     world = engine.world;
 
     
+    platform = new Ground(150,300,300,200)
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
